c250029a306f5f35643e6b03411ae0d6c49ad6db
David Butland <dgbutland@gmail.comm>
2019-07-02 08:47:06 +0100

    Tried to fix variable error but it looks like a lint problem

--- variables.tf
+++ variables.tf
@@ -1,11 +1,12 @@
-provider "aws" {
-  access_key = var.access_key
-  secret_key = var.secret_key
-  region     = var.region
-}
-
 variable "access_key" {}
 variable "secret_key" {}
 variable "region" {
   default = "eu-west-2"
 }
+
+provider "aws" { 
+	access_key = var.access_key
+  secret_key = var.secret_key
+  region     = var.region
+}
+
